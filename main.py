0f06571095e793c5c5e6c270d8d94492e8ba4a12
jayakvlr <52914511+jayakvlr@users.noreply.github.com>
2023-08-18 12:29:45 +0000

    added comments

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def add(a, b):
-    return a + b
+"""
+This module provides a simple function for addition.
+"""
+
+
+def add(num1, num2):
+    """
+    Add two numbers.
+
+    This function takes two numbers as input and returns their sum.
+
+    :param a: The first number.
+    :param b: The second number.
+    :return: The sum of `a` and `b`."""
+    return num1+ num2
 
 
 print(add(1, 1))
--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
+"""this is test code"""
 from main import add
 
-
 def test_add():
+    """this test is code to add"""
     assert 2 == add(1, 1)
